2fae6dbf1f8c04c4a49950fd460b5a4a97b172b1
Lalezka Duque <lalezkaduque@gmail.com>
2017-02-06 23:18:14 -0400

    Esqueleto de las clases necesarias para el AST

---
+++ Entrega2/clasesParser.rb
@@ -0,0 +1,68 @@
+class TablaSimbolos
+end
+
+class Instruccion
+end
+
+class Condicional
+end
+
+class Asignacion
+end
+
+class Declaracion
+end
+
+class RepeticionI
+end
+
+class RepeticionD
+end
+
+class Entrada
+end
+
+class Salida
+end
+
+class Variable
+end
+
+class Tipo
+end
+
+class Literal
+end
+
+class LiteralNumerico
+end
+
+class LiteralBooleano
+end
+
+class Imagen
+end
+
+class Expresion
+end
+
+class ExpresionBin
+end
+
+class ExpresionU
+end
+
+class ListaExpresiones
+end
+
+class Identificador
+end
+
+class Bloque
+end
+
+class Alcance
+end
+
+class Funcion
+end
